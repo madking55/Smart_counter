92c0fbbbc8d2015f550111b34b455307cc20a37b
madking55 <madking55@gmail.com>
2021-01-28 22:18:01 +0100

    Refactor runner file

--- list_webpages.rb
+++ list_webpages.rb
@@ -1,9 +1,26 @@
+require_relative 'lib/reader'
+require_relative 'lib/parser'
 require_relative 'lib/counter'
 
-webpages = Counter.new(ARGV[0])
 
-puts "Webpages with most views:"
-  webpages.all_views.each { |page| puts "#{page[0]} - #{page[1]} views" }
-puts '-------------------------------'
-puts "Webpages with most unique views:"
-  webpages.unique_views.each  { |page| puts "#{page[0]} - #{page[1]} unique views"}
+file = ARGV[0]
+
+if file.nil? 
+  puts 'Please provide a file name!'
+else 
+  data = Reader.new(file).read_file
+
+  if data.nil?
+    puts 'Log file does not exist!'
+  elsif data.empty?
+    puts "Log file is empty!" 
+  else
+    logs = Parser.new(data).parse
+    webpages = Counter.new(logs)
+    puts "Webpages with most views:"
+      webpages.all_views.each { |page| puts "#{page[0]} - #{page[1]} views" }
+    puts '-------------------------------'
+    puts "Webpages with most unique views:"
+      webpages.unique_views.each  { |page| puts "#{page[0]} - #{page[1]} unique views"}
+  end
+end

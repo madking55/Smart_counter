1c3c5d9aa067938e87a0b6c7200a75635349f21a
madking55 <madking55@gmail.com>
2021-01-28 21:50:00 +0100

    Refactor Parser and specs

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,24 +1,19 @@
 class Parser
-  # file = ARGV[0]
 
-  def parse(file)
-    return 'Log file does not exist!' unless file_provided?(file)
-    data = File.readlines(file)
-    return "Log file is empty!" if data.empty?
-    logs = data.map { |log| log.chomp.split(' ') }
-    webpages_with_views = Hash.new { |hash, key| hash[key] = [] }
+  def initialize(data)
+    @data = data
+  end
+
+  def parse
+    logs = @data.map { |log| log.chomp.split(' ') }
+    parsed_logs = Hash.new { |hash, key| hash[key] = [] }
 
     logs.each do |log|
-      webpage = webpages_with_views[log[0]]
+      webpage = parsed_logs[log[0]]
       view = log[1]
       webpage << view
     end
-    webpages_with_views
-  end
-
-  private
-
-  def file_provided?(file)
-    File.exist?(file)
+    
+    parsed_logs
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,45 +1,26 @@
 require 'parser'
+require 'reader'
 require 'pry'
 
 RSpec.describe Parser do
   describe '.parse' do
+    it 'returns a hash of webpages with views' do
+      
+      file = 'spec/sample.log'
+      reader = Reader.new(file)
+      data = reader.read_file
+      parser = Parser.new(data)
+      result = {
+                  '/help_page/1' => ['126.318.035.038', '722.247.931.582', '722.247.931.582'],
+                  '/contact' => ['184.123.665.067'],
+                  '/home' => ['184.123.665.067', '235.313.352.950'],
+                  '/about/2' => ['444.701.448.104', '444.701.448.104', '444.701.448.104'],
+                  '/about' => ['061.945.150.735']
+                }
 
-    context 'if log file exists' do
-
-      it 'returns a hash of webpages with views' do
-        
-        file = 'spec/sample.log'
-        parser = Parser.new
-        result = {
-                    '/help_page/1' => ['126.318.035.038', '722.247.931.582', '722.247.931.582'],
-                    '/contact' => ['184.123.665.067'],
-                    '/home' => ['184.123.665.067', '235.313.352.950'],
-                    '/about/2' => ['444.701.448.104', '444.701.448.104', '444.701.448.104'],
-                    '/about' => ['061.945.150.735']
-                  }
-
-        expect(parser.parse(file)).to be_instance_of(Hash)
-        expect(parser.parse(file)).to eq(result)
-        expect(parser.parse(file).values.flatten.count).to eq(10)
-      end
-    
-      it 'displays empty logs message if file empty' do
-
-        file = 'spec/empty.log'
-        parser = Parser.new
-
-        expect(parser.parse(file)).to eq("Log file is empty!")
-      end
-    end
-
-    context 'if log file does not exist' do
-      it 'displays error message' do
-
-        file = 'spec/fake.log'
-        parser = Parser.new
-
-        expect(parser.parse(file)).to eq('Log file does not exist!')
-      end
+      expect(parser.parse).to be_instance_of(Hash)
+      expect(parser.parse).to eq(result)
+      expect(parser.parse.values.flatten.count).to eq(10)
     end
   end
 end

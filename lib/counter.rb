a641601d267c4a1deb83e464a138b38a6f9d9512
madking55 <madking55@gmail.com>
2021-01-28 22:16:58 +0100

    Refactor Counter and specs

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,19 +1,23 @@
-require_relative 'parser'
-
 class Counter
-  def initialize(file)
-    @webpages_with_views = Parser.new.parse(file)
+  def initialize(logs)
+    @webpages_with_views = logs
+    @all_views = {}
+    @unique_views = {}
   end
 
-  def all_views
-    all_views = {}
-    @webpages_with_views.each { |webpage, views| all_views[webpage] = views.count }
-    all_views.sort_by { |key, value| value }.reverse
+  def list_all_views
+    @webpages_with_views.each { |webpage, views| @all_views[webpage] = views.count }
+    sort_views(@all_views)
   end
 
-  def unique_views
-    unique_views = {}
-    @webpages_with_views.each { |webpage, views| unique_views[webpage] = views.uniq.count }
-    unique_views.sort_by { |key, value| value }.reverse
+  def list_unique_views
+    @webpages_with_views.each { |webpage, views| @unique_views[webpage] = views.uniq.count }
+    sort_views(@unique_views)
+  end
+
+  private
+
+  def sort_views(views)
+    views.sort_by { |key, value| value }.reverse
   end
 end
--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,26 +1,32 @@
 require 'counter'
+require 'parser'
+require 'reader'
 require 'pry'
 
 RSpec.describe Counter do
-  before :each do
+  before :all do
     @file = 'spec/sample.log'
-    @counter = Counter.new(@file)
+    reader = Reader.new(@file)
+    data = reader.read_file
+    parser = Parser.new(data)
+    logs = parser.parse
+    @counter = Counter.new(logs)
   end
 
-  describe '.all_views' do
-    it 'returns lsit of webpages with most views ordered by most views' do
+  describe '.list_all_views' do
+    it 'returns list of webpages with most views ordered by most views' do
       result = [["/about/2", 3], ["/help_page/1", 3], ["/home", 2], ["/about", 1], ["/contact", 1]]
 
-      expect(@counter.all_views).to eq(result)
-      expect(@counter.all_views.to_h.values.sum).to eq(File.readlines(@file).count)
+      expect(@counter.list_all_views).to eq(result)
+      expect(@counter.list_all_views.to_h.values.sum).to eq(File.readlines(@file).count)
     end
   end
 
-  describe '.unique_views' do
+  describe '.list_unique_views' do
     it 'returns list of webpages with most unique page views ordered by most views' do
       result = [["/home", 2], ["/help_page/1", 2], ["/about", 1], ["/about/2", 1], ["/contact", 1]]
 
-      expect(@counter.unique_views).to eq(result)
+      expect(@counter.list_unique_views).to eq(result)
     end
   end
 end
